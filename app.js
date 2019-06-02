bd1735acbf2154cdb720edfb4aff2d8f123bbd44
binieth <abigiyabiniyam@gmail.com>
2019-06-02 16:42:39 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var msg = 'Sign Up to our Newsletter to recieve 10% off from your next order!';
+
+function newMessage(){
+var e = document.getElementById('message');
+e.textContent = msg;
+}
+newMessage();
