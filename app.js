928c9443de7852ae44a6aa5569bebee64bed3a77
binieth <abigiyabiniyam@gmail.com>
2019-06-02 16:58:23 -0400

    new commit

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var msg = 'Sign Up to our Newsletter to recieve 10% off from your next order!';
+var grtg = 'Hello Please Introduce your Name:'
 
 function newMessage(){
 var e = document.getElementById('message');
+var g = document.getElementById('greeting');
 e.textContent = msg;
+g.textContent = grtg;
 }
 newMessage();
